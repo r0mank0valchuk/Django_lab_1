099ecb0c52ad9559bb0f5991555a47d0b53a9f06
Roman <kovalchuk.roman@chnu.edu.ua>
2025-10-27 03:22:05 +0200

    Update Django project

--- lab1/lab1/settings.py
+++ lab1/lab1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog'
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,16 @@ WSGI_APPLICATION = 'lab1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lab1_db',
+        'USER': 'lab1_user',
+        'PASSWORD': 'ilovedjango',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
